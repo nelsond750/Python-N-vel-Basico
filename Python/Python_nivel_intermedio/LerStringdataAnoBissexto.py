39fedb938cf195ecb8e09ec54d8a70ffd8751a88
Nelson Salvador <nelsond750@gmail.com>
2025-10-29 14:59:27 +0100

    Alterção-29/10/2025

--- Python/Python_nivel_intermedio/LerStringdataAnoBissexto.py
+++ Python/Python_nivel_intermedio/LerStringdataAnoBissexto.py
@@ -4,16 +4,14 @@ S = input('Digite a string no formato aaaammdd: ')
 if len(S) == 8 and S.isnumeric():
 #Fatiamento para extrair as partes (se a validação for True)
     ano = S[:4]
-    if S[:4]%4 == 0:
-        mes = S[4:6]
-        dia = S[6:]
-        print('O ano é bissexto')
+    mes = S[4:6]
+    dia = S[6:]
+    if int(ano) % 4 == 0:  
+        print('O ano é bissexto')   
+#Saída no formato dd/mm/aaaa~     
     else:
-        print('Não é bissexto')
-#Saída no formato dd/mm/aaaa~
-    
-    print(f'A data fornecida é: {dia}/{mes}/{ano}')
+        print('O ano não é bissexto') 
+    print(f'A data fornecida é: {dia}/{mes}/{ano}') 
 else:
 #Mensagem de erro (se a validação for False)
     print('Erro! apenas numeros inteiros.') 
-
---
+++ Python/Python_nivel_intermedio/QuantidadeInteiroAleatório.py
@@ -0,0 +1,19 @@
+from random import randint
+Lst = []
+Qtde = int(input('Digite a quantidade: '))
+for i in range(Qtde):
+    a = randint(1, 100)
+    Lst.append(a)
+print('\n Antes de eliminar repetição')
+print(f'{Lst}')
+X = 1
+while X > 0:
+    X = int(input('Digite um valor '))
+    if X in Lst:
+        while X in Lst:
+                Lst.remove(X)
+        print(f'{Lst}\n E a quantidade é:{Lst.count(X) }')
+        
+    else:
+        print(f'\n O número {X} não está na lista. Fim do programa.')
+        break
